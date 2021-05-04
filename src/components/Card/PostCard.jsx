155c92a9673f7b229a1a1f159dad4fa893c0d8e6
robertoargs <48626619+robertoargs@users.noreply.github.com>
2021-05-03 22:19:08 -0500

    Project Updated

---
+++ src/components/Card/PostCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button
+} from 'reactstrap';
+
+import './BlogCard.css';
+import { Link } from 'react-router-dom';
+
+function BlogCard(props) {
+  return (
+    <>
+      <Card className="propsCard">
+        <CardImg top className="cardImg" src={props.src} alt={props.alt} />
+        <CardBody>
+          <CardTitle tag="h5">{props.title}</CardTitle>
+          {/*<CardSubtitle tag="h6" className="mb-2 text-muted">{props.subTitle}</CardSubtitle>*/}
+          <CardText>{props.text.length  <=100 ? props.text : `${props.text.substring(0,100)}...`  }</CardText>
+        </CardBody>
+      </Card>
+    </>
+  );
+}
+
+export default BlogCard;
